816de88a67e02bc68725055fc0f1f8175514b32f
rwxp <sdrivert@hotmail.com>
2023-05-18 13:17:51 -0500

    Pruebota

--- authors.test.js
+++ authors.test.js
@@ -1,19 +1,15 @@
 const supertest = require('supertest');
-const { app, server } = require('../backend-app/index');
+const { app, server } = require('./backend-app/index');
 const api = supertest(app);
 // Antes de todas las pruebas
 
 // Pruebas de GET /authors...
 test('debe retornar todos los autores', async () => {
   await api.get('/authors').expect(200);
-
   // expect(response.status).toEqual(200);
   // Resto del código de la prueba...
 });
 
-test('debe retornar un libro específico', async () => {
-  await api.get('/authors/1').expect(200);
-});
 afterAll(() => {
   server.close();
 });
