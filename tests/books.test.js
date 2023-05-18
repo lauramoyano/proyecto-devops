720bd3dde30eda6bc71c979c99a10819d186e54f
JoshuaAAX <joshua.chicame@correounivalle.edu.co>
2023-05-17 19:47:08 -0500

    add comments books.test

--- tests/books.test.js
+++ tests/books.test.js
@@ -22,14 +22,14 @@ describe('books.router', () => {
     test('debe retornar un libro específico', async () => {
       const requiredProperties = ["title", "score", "published_date", "id_category", "id_author", "id_editorial"];
 
+      //prueba obtener un libro de un id
       const response1 = await request(API_BASE_URL ).get('/books/4');
       expect(response1.status).toBe(200);
       expect(response1.headers['content-type']).toMatch(/application\/json/);
       expect(response1.body).toBeInstanceOf(Object);
       expect(response1.body).toHaveProperty(...requiredProperties);
 
-
-
+     //prueba obtener un mensaje de libro no encontrado al pasar un libro inexistente
       const response2 = await request('localhost:7000').get('/books/-1');
       expect(response2.status).toBe(404);
       expect(response2.body).toHaveProperty('message', 'Book no found--get');
@@ -83,17 +83,20 @@ describe('books.router', () => {
       expect(response.status).toBe(200);
       expect(response.headers['content-type']).toMatch(/application\/json/);
       expect(response.body).toBeInstanceOf(Object);
+      expect(response.body).toMatchObject(book);
 
     });
   });
 
 
   describe('DELETE /authors/:id', () => {
-    it('debe eliminar un libroe específico', async () => {
+    it('debe eliminar un libro específico', async () => {
      
+    //prueba eliminar el ultimo libro creado en la prueba
      const response1 = await request(API_BASE_URL ).delete(`/books/${id_last_book}`);
      expect(response1.status).toBe(204);
 
+     //prueba eliminar un libro inexistente
      const response2 = await request(API_BASE_URL ).delete(`/books/-1`);
      expect(response2.status).toBe(404);
 
